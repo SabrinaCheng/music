58acfaa030b2fcc562132f61c65031e19d0b6a6e
little tree <littletree@littlede-MacBook-Pro.local>
2020-06-16 19:11:34 -0400

    kafka test case

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,54 +1,27 @@
-from confluent_kafka import Producer, KafkaError
-import json
-import ccloud_lib
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+from json import dumps
 
+bootstrap_servers = '10.0.0.7:9092'
 
-if __name__ == '__main__':
+producer = KafkaProducer(bootstrap_servers=bootstrap_servers,
+                        value_serializer=lambda x: 
+                         dumps(x).encode('utf-8')
+                        )
 
-    # Read arguments and configurations and initialize
-    args = ccloud_lib.parse_args()
-    config_file = args.config_file
-    topic = args.topic
-    conf = ccloud_lib.read_ccloud_config(config_file)
+print('ss')
 
-    # Create Producer instance
-    producer = Producer({
-        'bootstrap.servers': conf['bootstrap.servers'],
-        'sasl.mechanisms': conf['sasl.mechanisms'],
-        'security.protocol': conf['security.protocol'],
-        'sasl.username': conf['sasl.username'],
-        'sasl.password': conf['sasl.password'],
-    })
+for e in range(10):
+    data = {'number' : e}
+    producer.send('test', value=data)
 
-    # Create topic if needed
-    ccloud_lib.create_topic(conf, topic)
+# for i in range(3):
+#     data = {'num': i}
+#     try: 
+#         producer.send('test', value=data.encode('utf-8'))
+#     except KafkaError as e:
+#         print(e)
 
-    delivered_records = 0
+print('sss')
 
-    # Optional per-message on_delivery handler (triggered by poll() or flush())
-    # when a message has been successfully delivered or
-    # permanently failed delivery (after retries).
-    def acked(err, msg):
-        global delivered_records
-        """Delivery report handler called on
-        successful or failed delivery of message
-        """
-        if err is not None:
-            print("Failed to deliver message: {}".format(err))
-        else:
-            delivered_records += 1
-            print("Produced record to topic {} partition [{}] @ offset {}"
-                  .format(msg.topic(), msg.partition(), msg.offset()))
-
-    for n in range(10):
-        record_key = "alice"
-        record_value = json.dumps({'count': n})
-        print("Producing record: {}\t{}".format(record_key, record_value))
-        producer.produce(topic, key=record_key, value=record_value, on_delivery=acked)
-        # p.poll() serves delivery reports (on_delivery)
-        # from previous produce() calls.
-        producer.poll(0)
-
-    producer.flush()
-
-    print("{} messages were produced to topic {}!".format(delivered_records, topic))
+producer.flush()
