c98b4477adc0d8d6966f54f0e02d0cb82b2196c0
little tree <littletree@littlede-MacBook-Pro.local>
2020-06-22 21:19:43 -0700

    add comment

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -27,50 +27,57 @@ def update_record(dict_data, ts, wid, uid, hr):
     dict_data['avg_hr'].append(round(sum(dict_data['hr']) / (len(dict_data['hr'])), 2))
     return dict_data
 
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('Please enter Redis password and Kafka topic')
 
-password = sys.argv[1]
-topic = sys.argv[2]
+    password = sys.argv[1]
+    topic = sys.argv[2]
 
-consumer = KafkaConsumer(
-     topic,
-     bootstrap_servers=['10.0.0.7:9092'],
-     auto_offset_reset='earliest',
-     value_deserializer=lambda m: json.loads(m.decode('utf-8')), 
-     enable_auto_commit=True,
-     )
+    # connect to Kafka
+    consumer = KafkaConsumer(
+        topic,
+        bootstrap_servers=['10.0.0.7:9092'],
+        auto_offset_reset='earliest',
+        value_deserializer=lambda m: json.loads(m.decode('utf-8')), 
+        enable_auto_commit=True,
+        )
 
-r = redis.Redis(
-    host='ec2-34-219-119-131.us-west-2.compute.amazonaws.com',
-    port=6379, 
-    password=password)
+    # connect to Redis
+    r = redis.Redis(
+        host='ec2-34-219-119-131.us-west-2.compute.amazonaws.com',
+        port=6379, 
+        password=password)
 
+    # process message from Kafka
+    for message in consumer:
+        message = message.value
+        ts, wid, uid, hr = message.values()
+        print(ts, wid, uid, hr)
+        # check heart rate within normal range
+        if MIN_HEART_RATE <= hr <= MAX_HEART_RATE:
+            
+            # check user exists or not
+            user_data = r.get(uid)
+            print(uid, user_data)
+            
+            # existing user
+            if user_data:
+                user_data = json.loads(user_data)
+                # new workout for both existing and new users
+                if user_data['id'] != wid:
+                    add_record(user_data, ts, wid, uid, hr)
+                # same workout, new hr
+                else:
+                    update_record(user_data, ts, wid, uid, hr)
 
-for message in consumer:
-    message = message.value
-    ts, wid, uid, hr = message.values()
-    print(ts, wid, uid, hr)
-    # check heart rate within normal range
-    if MIN_HEART_RATE <= hr <= MAX_HEART_RATE:
-        
-        # check user exists or not
-        user_data = r.get(uid)
-        print(uid, user_data)
-        
-        # existing user
-        if user_data:
-            user_data = json.loads(user_data)
-            # new workout for both existing and new users
-            if user_data['id'] != wid:
+            else: # user doesn't exist
+                # add new user
+                user_data = {}
                 add_record(user_data, ts, wid, uid, hr)
-            # same workout, new hr
-            else:
-                update_record(user_data, ts, wid, uid, hr)
-        else: # user doesn't exist
-            # add new user
-            user_data = {}
-            add_record(user_data, ts, wid, uid, hr)
-    # add or update to redis
-    print('======', uid, json.dumps(user_data))
-    r.set(uid, json.dumps(user_data))
+
+        # add or update to redis
+        print('======', uid, json.dumps(user_data))
+        r.set(uid, json.dumps(user_data))
 
 
