b4c48cbd5daaeed4ceb1dfbf5f05ced733434be1
little tree <littletree@littlede-MacBook-Pro.local>
2020-06-24 22:30:38 -0700

    clean up files

--- dash/redis2dash.py
+++ dash/redis2dash.py
@@ -1,4 +1,20 @@
+import sys
 import dash_core_components
+import redis
 
 
-print(dash_core_components.__version__)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('Please enter Redis password')
+
+    password = sys.argv[1]
+
+    # connect to Redis
+    r = redis.Redis(
+        host='ec2-34-219-119-131.us-west-2.compute.amazonaws.com',
+        port=6379, 
+        password=password)
+    
+    for k in r.keys():
+        print(k)
