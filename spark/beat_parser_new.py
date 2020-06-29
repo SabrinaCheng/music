10a39dbbd432819fac13d13ab12e2c34c82b8b81
little tree <littletree@littlede-mbp.lan>
2020-06-28 23:11:43 -0700

    input data in 10 mins

--- spark/beat_parser_new.py
+++ spark/beat_parser_new.py
@@ -1,13 +1,16 @@
 import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import arrays_zip, explode, col
+from pyspark.sql.functions import size, udf, array, lit, expr, rand, arrays_zip, explode, col
 from datetime import datetime
 
 """parse row fitrec data to kafka procuder message 
 with format: timestamp, id, userId, heart_rate"""
 
+# time window: 10 mins
+TIME_WINDOW = 600
+
 if __name__ == "__main__":
-    output_file = 's3a://fitrec/proper/endomondoHR_proper_ready.json'
+    # output_file = 's3a://fitrec/proper/endomondoHR_proper_tenminutes.json'
 
     # init spark session
     spark = SparkSession\
@@ -20,13 +23,19 @@ if __name__ == "__main__":
     input_json_path = 's3a://fitrec/proper/endomondoHR_proper.json'
     fitDF = spark.read.json(input_json_path)
     # fitDF.printSchema()
+ 
 
     # get beat subset and flatten data 
     beatDF = (fitDF
-            .withColumn("tmp", arrays_zip("timestamp", "heart_rate"))
+            .withColumn("o_ts", array([lit(fitDF["timestamp"].getItem(0))] * 500))
+            .withColumn("rand", array([lit(rand() * TIME_WINDOW).cast("int")] * 500))
+            .withColumn("new_time", 
+                        expr("transform(arrays_zip(timestamp, o_ts, rand), x -> x.timestamp - x.o_ts + x.rand)"))
+            .withColumn("tmp", arrays_zip("new_time", "heart_rate"))
             .withColumn("tmp", explode("tmp"))
-            .select(col("tmp.timestamp"), "id", "userId", col("tmp.heart_rate"))
-            .sort(col("timestamp")))
+            .select(col("tmp.new_time"), "id", "userId", col("tmp.heart_rate"))
+            .filter(col('new_time') >= 0)
+            .sort(col("new_time")))
     beatDF.select("*").show(truncate=True)
     beatDF.describe().show()
 
@@ -38,7 +47,8 @@ if __name__ == "__main__":
     print('===============================================================')
 
     # write to json
-    beatDF.write.json(output_file)
+    # beatDF.write.json(output_file)
+    # beatDF.coalesce(3).write.format('json').save(output_file)
 
     # stop spark session
     spark.stop()
